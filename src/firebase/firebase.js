67f0b91f3d2d0e44bd97d9796e48727c236889e5
Thierno-ba04 <thiernoba365@gmail.com>
2024-07-01 10:43:34 +0000

    first commit

---
+++ src/firebase/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCJIxKtl3E9_BdsYUXHR3YIBwQj8du5aBk",
+  authDomain: "tache-21-1e807.firebaseapp.com",
+  projectId: "tache-21-1e807",
+  storageBucket: "tache-21-1e807.appspot.com",
+  messagingSenderId: "297352485425",
+  appId: "1:297352485425:web:bb26bca311b5e669fe5310"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
