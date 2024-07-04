bc35bb566d0806f37336b0578e83b9bec9f2ab26
Thierno-ba04 <thiernoba365@gmail.com>
2024-07-04 17:17:59 +0000

    first commit

--- src/pages/pagesCoach/programme/Programme.js
+++ src/pages/pagesCoach/programme/Programme.js
@@ -1,14 +1,122 @@
-import React from 'react';
+import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import "./programme.css";
 
 const Programme = () => {
-    return ( 
-        <div>
-            <p>
-                ldefefk
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptates ipsam dolores quam maxime praesentium! Dolore ea sint commodi, voluptas deleniti, quibusdam molestiae a veritatis, optio saepe officia sit placeat corrupti.
-            </p>
+  return (
+    <Container>
+      <div className="d-flex justify-content-center">
+        <div
+          className="bg-dark "
+          style={{ color: "white", width: "55%", borderRadius: "20px" }}
+        >
+          <h1 className="text-center">Compétences</h1>
         </div>
-     );
-}
- 
+      </div>
+      <Row>
+        <Col md={6}>
+          <div
+            className="mere-web mt-5 d-flex justify-content-center"
+            style={{ borderRadius: "25px", height: "14rem" }}
+          >
+            <div className=" " style={{ color: "" }}>
+              <h5 className="mt-3">Web Developpement</h5>
+              <div className="icons d-flex justify-content-center mt-4">
+                <i class="fa-solid fa-laptop-code"></i>
+              </div>
+              <p>plus de 2 millions d'étudiants</p>
+              <div className="eye-pen d-flex justify-content-center gap-5">
+                <div>
+                  <i class="fa-solid fa-eye" style={{color:"yellow"}}></i>
+                </div>
+                <div>
+                  <i class="fa-solid fa-pencil" style={{color:"blue"}}></i>
+                </div>
+                <div>
+                  <i class="fa-solid fa-trash" style={{color:"red"}}></i>
+                </div>
+              </div>
+            </div>
+          </div>
+        </Col>
+        <Col md={6}>
+          <div
+            className="mere-web mt-5 d-flex justify-content-center"
+            style={{ borderRadius: "25px", height: "14rem" }}
+          >
+            <div className=" " style={{ color: "" }}>
+              <h5 className="mt-3 ms-5">Bureautique</h5>
+              <div className="icons d-flex justify-content-center mt-4">
+              <i class="fa-solid fa-book"></i>              </div>
+              <p>plus de 2 millions d'étudiants</p>
+              <div className="eye-pen d-flex justify-content-center gap-5">
+                <div>
+                  <i class="fa-solid fa-eye" style={{color:"yellow"}}></i>
+                </div>
+                <div>
+                  <i class="fa-solid fa-pencil" style={{color:"blue"}}></i>
+                </div>
+                <div>
+                  <i class="fa-solid fa-trash" style={{color:"red"}}></i>
+                </div>
+              </div>
+            </div>
+          </div>
+        </Col>
+      </Row>
+      <Row>
+        <Col md={6}>
+          <div
+            className="mere-web mt-5 d-flex justify-content-center"
+            style={{ borderRadius: "25px", height: "14rem" }}
+          >
+            <div className=" " style={{ color: "" }}>
+              <h5 className="mt-3 ms-5">Marketing</h5>
+              <div className="icons d-flex justify-content-center mt-4">
+              <i class="fa-solid fa-cart-shopping"></i>              </div>
+              <p>plus de 2 millions d'étudiants</p>
+              <div className="eye-pen d-flex justify-content-center gap-5">
+                <div>
+                  <i class="fa-solid fa-eye" style={{color:"yellow"}}></i>
+                </div>
+                <div>
+                  <i class="fa-solid fa-pencil" style={{color:"blue"}}></i>
+                </div>
+                <div>
+                  <i class="fa-solid fa-trash" style={{color:"red"}}></i>
+                </div>
+              </div>
+            </div>
+          </div>
+        </Col>
+        <Col md={6}>
+          <div
+            className="mere-web mt-5 d-flex justify-content-center"
+            style={{ borderRadius: "25px", height: "14rem" }}
+          >
+            <div className=" " style={{ color: "" }}>
+              <h5 className="mt-3 ms-5">Graphic Design</h5>
+              <div className="icons d-flex justify-content-center mt-4">
+                <i class="fa-solid fa-laptop-code"></i>
+              </div>
+              <p>plus de 2 millions d'étudiants</p>
+              <div className="eye-pen d-flex justify-content-center gap-5">
+                <div>
+                  <i class="fa-solid fa-eye" style={{color:"yellow"}}></i>
+                </div>
+                <div>
+                  <i class="fa-solid fa-pencil" style={{color:"blue"}}></i>
+                </div>
+                <div>
+                  <i class="fa-solid fa-trash" style={{color:"red"}}></i>
+                </div>
+              </div>
+            </div>
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
 export default Programme;
