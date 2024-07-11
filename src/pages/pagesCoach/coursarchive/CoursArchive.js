cee406c96f560beef168b0e4025c433f1a4a7ad2
Thierno-ba04 <thiernoba365@gmail.com>
2024-07-11 09:41:24 +0000

    first commit

--- src/pages/pagesCoach/coursarchive/CoursArchive.js
+++ src/pages/pagesCoach/coursarchive/CoursArchive.js
@@ -1,20 +1,9 @@
-import ReactPlayer from "react-player";
-
-const CoursArchive = ({ archivedVideos }) => {
-  return (
+const CoursArchive = () => {
+  return ( 
     <div>
-      <h1>Archived Videos</h1>
-      {archivedVideos.length === 0 ? (
-        <p>No videos archived yet.</p>
-      ) : (
-        archivedVideos.map((url, index) => (
-          <div key={index}>
-            <ReactPlayer url={url} />
-          </div>
-        ))
-      )}
+      
     </div>
-  );
-};
-
+   );
+}
+ 
 export default CoursArchive;
